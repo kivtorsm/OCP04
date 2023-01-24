2ae4ee98cc6da18c66bff100ca48586acf5e0187
kivtorsm <serradilla.victor@gmail.com>
2023-01-24 11:34:32 +0100

    docstrings and comments added to round.py

--- src/models/round.py
+++ src/models/round.py
@@ -18,33 +18,58 @@ class Round:
         return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
     def to_json(self):
+        """
+        Returns round object as a json string
+        """
         return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
     def set_start_time(self):
         """
-        Sets end date_time
+        Sets round start date_time
         """
         self.start_datetime = datetime.datetime.now().isoformat()
 
     def set_end_time(self):
         """
-        Sets end date_time
+        Sets round end date_time
         """
         self.end_datetime = datetime.datetime.now().isoformat()
 
-    def update_match_list(self, tournament, match_list):
+    def initialize_match_list(self, tournament, match_list):
+        """
+        Initializes the match list in the round object
+        """
         number_of_matches_per_round = tournament.get_number_of_matches_per_round()
+        # For each match, we update de list of matches
         for match_number in range(number_of_matches_per_round):
+            # A match list is formed of a list of couples that will confront in a match
             couple = match_list[match_number]
+            # Each couple is formed of a list of 2 players
             player1 = couple[0]
             player2 = couple[1]
+            # A match object is declared
             match = Match(player1, player2)
+            # The match is added to the round match list
             self.match_list.append(match)
 
     def get_match_list(self):
+        """
+        Returns the match list form the round
+        :return: list of matches
+        :rtype: list
+        """
         return self.match_list
 
     def update_round_data(self, tournament, round_number):
+        """
+        Updates the round data with the given round of a tournament data
+        :param tournament: tournament from which we get the data
+        :type tournament: tournament
+        :param round_number: round number data to be updated
+        :type round_number: int
+        :return: None
+        :rtype: None
+        """
         round_data = tournament.get_round_data(round_number)
         self.round_number = round_data['round_number']
         self.name = round_data['name']
