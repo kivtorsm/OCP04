a2f6fbf0b40a5ccf7a45aab38a8dc56d210eeaf2
kivtorsm <serradilla.victor@gmail.com>
2023-01-24 11:52:03 +0100

    docstrings added to tournament.py. Unused method in tournament.py erased

--- src/models/tournament.py
+++ src/models/tournament.py
@@ -34,23 +34,27 @@ class Tournament:
         return json.dumps(data, default=lambda o: o.__dict__, indent=4)
 
     def to_json(self):
+        """
+        Returns tournament object as a json string
+        :return: tournament object as a json str
+        :rtype: str
+        """
         return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
     def erase_file_data(self):
+        """
+        Erases tournament json file data
+        :return: None
+        :rtype: None
+        """
         with open(self.file_path, 'w'):
             pass
 
     def write_json_file(self):
         """
         Updates json tournament file
-
-        Parameters
-        -------
-        self
-
-        Returns
-        -------
-        None
+        :return: None
+        :rtype: None
         """
         try:
             with open(self.file_path, "r+") as file:
@@ -61,10 +65,22 @@ class Tournament:
             print(f'fichier {self.file_path} inexistant')
 
     def sign_in_player(self, player):
+        """
+        Signs-in the tournament a given player
+        :param player: player to be signed-in the tournament
+        :type player: player
+        :return: None
+        :rtype: None
+        """
         self.add_player_to_player_dict(player)
         self.write_json_file()
 
     def get_data_from_json_file(self):
+        """
+        Updates tournament object with json file data and returns it as an object
+        :return: file_data
+        :rtype: dict
+        """
         try:
             with open(self.file_path, "r") as file:
                 file_data = json.load(file)
@@ -86,60 +102,90 @@ class Tournament:
             print(f'fichier {self.file_path} inexistant')
             return None
 
-    # def get_player_dict_from_json(self):
-    #     """
-    #     Saves player list from json file to local memory
-    #     """
-    #     file_data = self.get_data_from_json_file()
-    #     self.player_dict = file_data['player_dict']
-
     def add_player_to_player_dict(self, player):
-        # self.get_player_dict_from_json()
+        """
+        Adds player to the tournament player dict
+        :param player: player to be added to the tournament
+        :type player: player
+        :return: None
+        :rtype: None
+        """
         self.player_dict[player.national_chess_identifier] = player
 
     def get_number_of_players(self):
-        # self.get_player_dict_from_json()
+        """
+        Returns the number of players signed-in the tournament
+        :return: Number of signed-in players
+        :rtype: int
+        """
         return len(self.player_dict)
 
     def get_number_of_rounds(self):
+        """
+        Returns the number of rounds to be played in the tournament
+        :return: number of rounds
+        :rtype: int
+        """
         return self.total_rounds
 
     def get_number_of_matches_per_round(self):
+        """
+        Returns the number of matches to be played in each round.
+        It's equals to the number of players divided by 2.
+        :return: number of players
+        :rtype: int
+        """
         return int(self.get_number_of_players()/2)
 
-    def update_match_list(self, tournament_round, couples_list):
-        # Get data from json
-        self.get_data_from_json_file()
-        # Update match list in the round object
-        tournament_round.update_match_list(self.get_number_of_matches_per_round(), couples_list)
-        # Update the tournament round list with the updated round object
-        self.round_list[tournament_round.round_number - 1] = tournament_round
-        # Save tournament to json file
-        self.write_json_file()
-
     def get_player_list(self):
+        """
+        Returns the list of players signed-in the tournament
+        :return: list of signed-in players
+        :rtype: list
+        """
         # self.get_player_dict_from_json()
         player_list = list(self.player_dict.keys())
         return player_list
 
     def get_round_list(self):
+        """
+        Returns the list of rounds to be played in the tournament
+        :return: list of rounds to be played in the tournament
+        :rtype: list
+        """
         self.get_data_from_json_file()
         return self.round_list
 
     def update_round_match_list(self, round_number, match_list):
+        """
+        Updates the list of matches of a given round with a new given list of matches
+        :param round_number: the round number where to update the list of matches
+        :type round_number: int
+        :param match_list: the updated list of matches to set in the round match list
+        :type match_list: list
+        :return: None
+        :rtype: None
+        """
         self.get_data_from_json_file()
         # create round object
         tournament_round = Round(round_number)
         # update round with json data
         tournament_round.update_round_data(self, round_number)
         # update round with input match_list
-        tournament_round.update_match_list(self, match_list)
+        tournament_round.initialize_match_list(self, match_list)
         # update tournament with round object
         self.round_list[round_number-1] = tournament_round
         # update json file
         self.write_json_file()
 
     def get_round_data(self, round_number):
+        """
+        Returns a given round data
+        :param round_number: number of the round for which to get the data
+        :type round_number: int
+        :return: round data
+        :rtype: dict
+        """
         round_data = self.round_list[round_number-1]
         return round_data
 
