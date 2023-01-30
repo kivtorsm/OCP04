13f7c3359e401458b6567bf4893431f535aef1f8
kivtorsm <serradilla.victor@gmail.com>
2023-01-30 15:45:14 +0100

    new : added current match indicator, current match increment function and is_finished round checking that all matches have been played

--- models/round.py
+++ models/round.py
@@ -7,37 +7,42 @@ from models.match import Match
 
 
 class Round:
-    def __init__(self, round_number):
+    def __init__(self, round_number, start_datetime=0, end_datetime=0, match_list=[], next_match=1):
         self.name = f"Round {round_number}"
         self.round_number = round_number
-        self.start_datetime = 0
-        self.end_datetime = 0
-        self.match_list = []
+        self.start_datetime = start_datetime
+        self.end_datetime = end_datetime
+        self.match_list = match_list
+        self.next_match = next_match
 
     def __str__(self):
-        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
-
-    def to_json(self):
-        """
-        Returns round object as a json string
-        """
         return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
     def set_start_time(self):
         """
         Sets round start date_time
+        :return: None
+        :rtype: None
         """
         self.start_datetime = datetime.datetime.now().isoformat()
 
     def set_end_time(self):
         """
         Sets round end date_time
+        :return: None
+        :rtype: None
         """
         self.end_datetime = datetime.datetime.now().isoformat()
 
-    def initialize_match_list(self, tournament, match_list):
+    def set_match_list(self, tournament, match_list):
         """
         Initializes the match list in the round object
+        :param tournament: tournament in which the round is played
+        :type tournament: Tournament
+        :param match_list: list of matche sof the round to be set for the round
+        :type match_list: list
+        :return: None
+        :rtype: None
         """
         number_of_matches_per_round = tournament.get_number_of_matches_per_round()
         # For each match, we update de list of matches
@@ -59,3 +64,25 @@ class Round:
         :rtype: list
         """
         return self.match_list
+
+    def increment_next_match(self):
+        """
+        Increments the next_match attribute to indicate which match is to be played next
+        :return: None
+        :rtype: None
+        """
+        self.next_match += 1
+
+    def is_round_finished(self, tournament):
+        """
+        Checks if the round has been finished by comparing the next_match indicator to the
+        number of matches per round of the tournament that depends on the number of players
+        :param tournament: tournament in which the round is played
+        :type tournament: Tournament
+        :return: result for test
+        :rtype: bool
+        """
+        if self.next_match > tournament.get_number_of_matches_per_round():
+            return True
+        else:
+            return False
