2800a8013a542d88b24cec45cd2e81caa0c285f7
kivtorsm <serradilla.victor@gmail.com>
2023-02-01 17:09:51 +0100

    new: launch tournament and add new players, controls if player exists in DB and if already signed-in

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,8 @@
 import os
 
 from models.json_file import ProgramData
+from models.tournament import Tournament
+from models.player import Player
 from views.view import View
 
 
@@ -18,7 +20,7 @@ class Controller:
         """
         self.view = view
 
-    def check_program_status(self, program_file: ProgramData):
+    def get_program_status(self, program_file: ProgramData):
         """
         Checks if the program file exists and returns a list of booleans indicating:
         1 - if the file is empty
@@ -27,14 +29,12 @@ class Controller:
         :return: list of booleans [file_is_empty, ongoing_tournament, existing_report_data]
         :rtype: list
         """
-        file_is_empty = True
         ongoing_tournament = False
         existing_report_data = False
-
         file_is_empty = self.program_file_is_empty()
         if not file_is_empty:
-            ongoing_tournament = self.ongoing_tournament(program_file)
-            existing_report_data = self.existing_player_data(program_file) or self.existing_tournament_data(program_file)
+            ongoing_tournament = self.ongoing_tournament_exists(program_file)
+            existing_report_data = self.player_data_exists(program_file) or self.tournament_data_exists(program_file)
         result = [file_is_empty, ongoing_tournament, existing_report_data]
         return result
 
@@ -68,25 +68,27 @@ class Controller:
         else:
             return False
 
-    def existing_tournament_data(self, program_file: ProgramData):
+    def tournament_data_exists(self, program_file: ProgramData):
         if not program_file.tournament_list:
             return False
         else:
             return True
 
-    def ongoing_tournament(self, program_file: ProgramData):
+    def get_last_tournament(self, program_file: ProgramData):
+        tournament_list = program_file.tournament_list
+        last_tournament = tournament_list[0]
+        return last_tournament
+
+    def ongoing_tournament_exists(self, program_file: ProgramData):
         result = False
-        if self.existing_tournament_data(program_file):
-            tournament_list = program_file.tournament_list
-            last_tournament = tournament_list[0]
+        if self.tournament_data_exists(program_file):
+            last_tournament = self.get_last_tournament(program_file)
             last_tournament_status = last_tournament.status
-            if last_tournament_status == "finished":
-                pass
-            else:
+            if not last_tournament_status == "finished":
                 result = True
         return result
 
-    def existing_player_data(self, program_file: ProgramData):
+    def player_data_exists(self, program_file: ProgramData):
         if not program_file.player_dict:
             return False
         else:
@@ -96,27 +98,104 @@ class Controller:
         choice = self.view.prompt_for_main_menu_choice(program_status)
         return choice
 
-    def run_menu_choice(self, menu_choice: int):
+    def run_main_menu_choice(self, program_file, menu_choice: int):
         options_list = [
-
+            "self.create_tournament(program_file)",
+            "self.run_tournament(program_file)",
+            "self.get_report_menu_choice()"
         ]
+        option_choice = options_list[menu_choice]
+        eval(option_choice)
 
-    def run_tournament(self, tournament_status: str):
+    def create_tournament(self, program_file: ProgramData):
+        tournament_data = self.view.prompt_for_tournament_creation()
+        tournament = Tournament(
+            tournament_data['name'],
+            tournament_data['place'],
+            tournament_data['start_date'],
+            tournament_data['end_date'],
+            tournament_data['description'],
+            rounds=tournament_data['rounds']
+        )
+        program_file.add_new_tournament(tournament)
+        self.run_tournament(program_file)
+
+    def get_report_menu_choice(self):
+        # TODO : code code code
         pass
 
-    def run_program(self):
-        program_file = self.charge_program_file()
-        program_status = self.check_program_status(program_file)
+    def already_signed_in_tournament(self, program_file: ProgramData, national_chess_identifier: str):
+        tournament = program_file.tournament_list[0]
+        player_list = tournament.player_list
+        if national_chess_identifier in player_list:
+            return True
+        else:
+            return False
+
+    def get_national_chess_identifier(self):
+        national_chess_identifier_correct = False
+        while not national_chess_identifier_correct:
+            try:
+                national_chess_identifier = self.view.prompt_for_national_chess_identifier()
+                assert len(national_chess_identifier) == 7
+                alpha = national_chess_identifier[:2]
+                numeric = national_chess_identifier[2:]
+                assert alpha.isalpha()
+                assert numeric.isnumeric()
+                national_chess_identifier_correct = True
+            except AssertionError:
+                print("Le format du numéro national d'échecs doit être de type AB12345")
+                # TODO : pas censé être ici le print ?
+
+        return national_chess_identifier
+
+    def sign_in_players(self, program_file: ProgramData):
+        national_chess_identifier = self.get_national_chess_identifier()
+        is_already_signed_in = self.already_signed_in_tournament(program_file, national_chess_identifier)
+        try:
+            assert is_already_signed_in
+            print(f"Le joueur {national_chess_identifier} est déjà inscrit au tournoi")
+        except AssertionError:
+            is_player_in_database = program_file.is_player_in_database(national_chess_identifier)
+            if not is_player_in_database:
+                player_data = self.view.prompt_for_player_data()
+                player = Player(
+                    first_name=player_data['first_name'],
+                    last_name=player_data['last_name'],
+                    birth_date=player_data['birth_date'],
+                    national_chess_identifier=national_chess_identifier
+                )
+                program_file.add_player(player)
+            tournament = self.get_last_tournament(program_file)
+            tournament.sign_in_player(national_chess_identifier)
+            program_file.update_ongoing_tournament(tournament)
+
+    def run_tournament(self, program_file: ProgramData):
+        tournament = self.get_last_tournament(program_file)
+        if tournament.status == "signing-in players":
+            self.sign_in_players(program_file)
+        else:
+            self.play_tournament()
+
+    def play_tournament(self):
+        # TODO : code code code
+        pass
+
+    def run_program(self, program_file):
+        program_status = self.get_program_status(program_file)
+        main_menu_choice = co
         main_menu_option = self.get_main_menu_choice(program_status)
+        # TODO : code code code
 
 
 def main():
     view = View()
     controller = Controller(view)
     program_file = controller.charge_program_file()
-    program_status = controller.check_program_status(program_file)
-    choice = controller.get_main_menu_choice(program_status)
-    print(choice)
+    while True:
+        program_status = controller.get_program_status(program_file)
+        main_menu_choice = controller.get_main_menu_choice(program_status)
+        controller.run_main_menu_choice(program_file, main_menu_choice)
 
 
 if __name__ == "__main__":
--- views/view.py
+++ views/view.py
@@ -1,11 +1,20 @@
 
 
 class View:
+    def program_file_created(self, path):
+        print(f"Fichier de programme créé : \n{path}")
+
+    def program_file_already_exists(self, path):
+        print(f"Nous avons trouvé le fichier de programme : \n{path}")
+
+    def program_file_empty(self):
+        print('Aucune information contenue dans le fichier de programme')
+
     def prompt_for_main_menu_choice(self, program_status):
         program_file_empty = program_status[0]
         ongoing_tournament = program_status[1]
         existing_report_data = program_status[2]
-        print(f"Bienvenue au menu principal")
+        print(f"\nBienvenue au menu principal")
         # if empty file the only option is to create a nez tournament
         if program_file_empty:
             input("Appuyez sur une touche pour démarrer un nouveau tournoi\n")
@@ -34,12 +43,65 @@ class View:
                 result = 2
         return result
 
-    def program_file_created(self, path):
-        print(f"Fichier de programme créé : \n{path}")
+    def prompt_for_tournament_creation(self):
+        tournament_data = {}
+        input_data = input("Veuillez saisir le nom du tournoi :\n")
+        tournament_data['name'] = input_data
+        input_data = input("Veuillez saisir le lieu du tournoi :\n")
+        tournament_data['place'] = input_data
+        print("Veuillez saisir la date de début du tournoi :")
+        start_date_day = input("Jour (jj) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        start_date_month = input("Mois (mm) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        start_date_year = input("Année (aaaa) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        input_data = f"{start_date_day}/{start_date_month}/{start_date_year}"
+        tournament_data['start_date'] = input_data
+        print("Veuillez saisir la date de fin du tournoi :")
+        end_date_day = input("Jour (jj) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        end_date_month = input("Mois (mm) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        end_date_year = input("Année (aaaa) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        # TODO: contrôler que date de fin > date de début
+        input_data = f"{end_date_day}/{end_date_month}/{end_date_year}"
+        tournament_data['end_date'] = input_data
+        rounds = input("Veuillez saisir le nombre de tours du tournoi (4 par défaut) :\n")
+        try:
+            input_data = int(rounds)
+        except ValueError:
+            input_data = 4
+            print("Tournoi à 4 tours")
+        finally:
+            tournament_data['rounds'] = input_data
+        input_data = input("Veuillez saisir une description du tournoi :\n")
+        tournament_data['description'] = input_data
 
-    def program_file_already_exists(self, path):
-        print(f"Nous avons trouvé le fichier de programme : \n{path}")
+        return tournament_data
 
-    def program_file_empty(self):
-        print('Aucune information contenue dans le fichier de programme')
+    def prompt_for_national_chess_identifier(self):
+        national_chess_identifier = input(
+            "Veuillez rentrer le numéro d'identification d'échecs du joueur à inscrire au tournoi :\n"
+        )
+        national_chess_identifier_upper = national_chess_identifier.upper()
+        return national_chess_identifier_upper
 
+    def prompt_for_player_data(self):
+        player_data = {}
+        input_data = input("Veuillez rentrer le prénom du joueur:\n")
+        player_data['first_name'] = input_data
+        input_data = input("Veuillez rentrer le nom de famille du joueur:\n")
+        player_data['last_name'] = input_data
+        print("Veuillez rentrer la date de naissance du joueur : \n")
+        birth_date_day = input("Jour (jj) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        birth_date_month = input("Mois (mm) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        birth_date_year = input("Année (aaaa) : ")
+        # TODO: contrôler qu'il s'agit du bon input
+        # TODO: contrôler que date de fin > date de début
+        input_data = f"{birth_date_day}/{birth_date_month}/{birth_date_year}"
+        player_data['birth_date'] = input_data
+        return player_data
