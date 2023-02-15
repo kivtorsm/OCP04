0db6ed68aa80eb496fa05a7977bf76048caf8676
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-02-15 12:06:10 +0700

    new: added main.py for running the program

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # coding: utf-8
 
-from models.json_file import ProgramData
-
 from controllers.control_program_file import ControlProgramFile
 from controllers.control_tournament import ControlTournament
 from controllers.control_round import ControlRound
 from controllers.control_tournament_player import ControlTournamentPlayer
-from controllers.tournament_controller import MainController
+from controllers.tournament_controller import TournamentController
 from controllers.reports_c import ReportsController
 from controllers.menu_c import MenuController
-
 
 from views.tournament_v import TournamentView
 from views.reports_v import ReportsView
@@ -19,13 +16,18 @@ from views.menu_v import MenuView
 
 
 def main():
+    """
+    Runs the program
+    :return: nothing
+    :rtype:
+    """
     view = TournamentView()
     round_view = RoundView()
     control_program_file = ControlProgramFile()
     control_tournament = ControlTournament()
     control_round = ControlRound(round_view)
     control_tournament_player = ControlTournamentPlayer()
-    tournament_controller = MainController(
+    tournament_controller = TournamentController(
         view=view,
         round_view=round_view,
         program_file_controls=control_program_file,
@@ -34,10 +36,15 @@ def main():
         tournament_player_controls=control_tournament_player
     )
 
-    tournament_controller.program_file = tournament_controller.program_file_controls.charge_program_file()
+    tournament_controller.program_file \
+        = tournament_controller.program_file_controls.charge_program_file()
 
     reports_view = ReportsView()
-    reports_controller = ReportsController(tournament_controller.program_file, control_program_file, reports_view)
+    reports_controller = ReportsController(
+        tournament_controller.program_file,
+        control_program_file,
+        reports_view
+    )
 
     menu_view = MenuView()
 
@@ -48,11 +55,14 @@ def main():
     )
 
     while True:
-        program_status = tournament_controller.program_file_controls.evaluate_program_status(
-            tournament_controller.program_file
-        )
+        program_status = tournament_controller.\
+            program_file_controls.\
+            evaluate_program_status(tournament_controller.program_file)
         main_menu_choice = menu_controller.get_main_menu_choice(program_status)
-        menu_controller.run_main_menu_choice(tournament_controller.program_file, main_menu_choice)
+        menu_controller.run_main_menu_choice(
+            tournament_controller.program_file,
+            main_menu_choice
+        )
 
 
 if __name__ == "__main__":
