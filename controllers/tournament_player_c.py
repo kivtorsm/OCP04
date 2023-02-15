b4722f0b190a8767854fea80e48300fd42d82698
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-02-15 17:34:58 +0700

    119 character line instead of 79

--- controllers/tournament_player_c.py
+++ controllers/tournament_player_c.py
@@ -26,7 +26,5 @@ class ControlTournamentPlayer:
         :rtype: bool
         """
         current_tournament = program_file.get_last_tournament()
-        player_in_tournament = current_tournament.get_player_in_tournament(
-            player1_national_chess_identifier)
-        return player2_national_chess_identifier in \
-            player_in_tournament.has_played
+        player_in_tournament = current_tournament.get_player_in_tournament(player1_national_chess_identifier)
+        return player2_national_chess_identifier in player_in_tournament.has_played
